c84b294911f7657f6612ca8a4243a8d25902ad4a
Nishan9t <pandeynisha9t@gmail.com>
2023-08-14 12:31:28 +0530

    sidebar is completed

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -1,10 +1,12 @@
 import React,{useEffect} from 'react'
 import { useStateProvider } from '../utils/StateProvider'
 import axios from 'axios';
+import { reducerCases } from '../utils/Constants';
+import { styled } from 'styled-components';
 
 export default function Playlists() {
 
-    const [{token,dispatch}]=useStateProvider();
+    const [{token,playlists},dispatch]=useStateProvider();
 
     useEffect(()=>{
 
@@ -17,13 +19,60 @@ export default function Playlists() {
             });
             const {items} =response.data
             // console.log(items)
-            
+            const playlists = items.map(({name,id})=>{
+                return {name,id};
+            });
+            // console.log(playlists)
+            dispatch({type:reducerCases.SET_PLAYLISTS,playlists:playlists});
         }
         getPlaylistData();
 
     },[token,dispatch])
 
   return (
-    <div>Playlists</div>
+    <Container>
+        <ul>
+            {
+                playlists.map(({name,id})=>{
+
+                    return(
+                        <li key={id}>{name}</li>
+                    )
+
+                })
+            }
+        </ul>
+    </Container>
   )
 }
+
+const Container =styled.div`
+
+    height:100%;
+    overflow:hidden;
+     ul{
+        list-style-type:none;
+        display:flex;
+        flex-direction:column;
+        gap:1rem;
+        padding:1rem;
+        height:52vh;
+        max-height:100%;
+        overflow:auto;
+        &::-webkit-scrollbar{
+            width:0.7rem;
+            &-thumb{
+                background-color:rgba(255,255,255,0.6);
+            }
+        }
+        li{
+            display:flex;
+            gap:1rem;
+            cursor:pointer;
+            transition:0.3s ease-in-out;
+            &:hover{
+                color:white;
+            }
+        }
+    }
+`;
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -2,6 +2,7 @@ import { reducerCases } from "./Constants";
 
 export const initialState={
     token:null,
+    playlists:[],
 };
 
 const reducer=(state,action)=>{
@@ -9,6 +10,11 @@ const reducer=(state,action)=>{
         case reducerCases.SET_TOKEN:{
             return {
                 ...state,token:action.token,
+            }
+        }
+        case reducerCases.SET_PLAYLISTS:{
+            return{
+                ...state,playlists:action.playlists,
             }
         }
         default:
